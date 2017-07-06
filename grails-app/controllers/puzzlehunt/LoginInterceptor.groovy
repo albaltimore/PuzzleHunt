fe2ce86ffd0196d757226e05531a1d8cf4f073ee
Aleks Tamarkin <aibolit@live.com>
2017-07-06 13:24:16 -0400

    add healthcheck

--- grails-app/controllers/puzzlehunt/LoginInterceptor.groovy
+++ grails-app/controllers/puzzlehunt/LoginInterceptor.groovy
@@ -4,7 +4,7 @@ package puzzlehunt
 class LoginInterceptor {
 
     public LoginInterceptor() {
-        matchAll().excludes controller: "login" excludes uri: "/error" excludes uri: "/notFound"
+        matchAll().excludes controller: "login" excludes uri: "/error" excludes uri: "/notFound" excludes controller: "healthcheck"
     }
 
     boolean before() {
