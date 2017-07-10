b4442b44cb6fdb3591d7c9791f5d2de7f3064be4
Stephanie Stattel <dharmaquark@StephaniesAir2.fios-router.home>
2017-07-10 00:54:40 -0400

    more hint queue features

--- grails-app/init/puzzlehunt/BootStrap.groovy
+++ grails-app/init/puzzlehunt/BootStrap.groovy
@@ -9,66 +9,6 @@ class BootStrap {
 
     def init = { servletContext ->
         loadFromPath()
-        
-        def firstRound = new Round(name: 'ROUND ONE', width: 10, height: 10)
-        
-        def a = new Puzzle(xCor:100, yCor:100, name:'Puzzle A', solution:"A", round: firstRound)
-        def b = new Puzzle(xCor:300, yCor:100, name:'Puzzle B', solution:"B", round: firstRound)
-        def c = new Puzzle(xCor:500, yCor:100, name:'Puzzle C', solution:"C", round: firstRound)
-        def d = new Puzzle(xCor:300, yCor:200, name:'Puzzle D', solution:"D", requiredPuzzles: [b], round: firstRound)
-        def e = new Puzzle(xCor:200, yCor:400, name:'Puzzle E', solution:"E", requiredPuzzles: [a, d], round: firstRound)
-        def f = new Puzzle(xCor:400, yCor:400, name:'Puzzle F', solution:"F", requiredPuzzles: [c, d], round: firstRound)
-        def g = new Puzzle(xCor:400, yCor:500, name:'Puzzle G', solution:"G", requiredPuzzles: [f], round: firstRound)
-        def h = new Puzzle(xCor:200, yCor:600, name:'Puzzle H', solution:"H", requiredPuzzles: [e, g], round: firstRound)
-        def i = new Puzzle(xCor:300, yCor:800, name:'Puzzle I', solution:"I", requiredPuzzles: [g, h], round: firstRound)
-
-        a.save()
-        b.save()
-        c.save()
-        d.save()
-        e.save()
-        f.save()
-        g.save()
-        h.save()
-        i.save()
-
-        def pl = new Player(name:"Aleks", password:"1", role: "HINTER")
-        def ss = new Player(name: "Stephanie", password:"2", role: "HINTER")
-        def uu = new Player(name: "Ania", password:"0", role: "PLAYER")
-        def dd = new Player(name: "Josh", password:"2", role: "HINTER")
-        def vv = new Player(name: "Ben", password:"2", role: "HINTER")
-
-        pl.save()
-        ss.save()
-        uu.save()
-        dd.save()
-        vv.save()
-        
-        def today = new Date().parse('yyyy/MM/dd', '2017/01/30')
-        def yesterday = new Date().parse('yyyy/MM/dd', '2017/01/29')
-        def oldest = new Date().parse('yyyy/MM/dd', '2001/01/29')
-        
-        def h_a = new Hint(player:pl, puzzle:a, owner: ss, lastOwner: uu, question:"I need help with this", notes: "NOTES", createTime: today)
-        def h_b = new Hint(player:pl, puzzle:b, owner: uu, question:"I need help again", notes:"NOTES", createTime: today)
-        def h_c = new Hint(player:pl, puzzle:c, owner:pl, question:"I need more help", notes:"NOTES", createTime: today)
-        def h_d = new Hint(player:pl, puzzle:d, owner:uu, question:"I have another question", notes:"NOTES", createTime: today)
-        def h_e = new Hint(player:pl, puzzle:e, owner:uu, question:"I have another question", notes:"NOTES", createTime: today)
-        def h_f = new Hint(player:pl, puzzle:a, owner:dd, question:"I have another question", notes:"NOTES", createTime: yesterday)
-        def h_g = new Hint(player:pl, puzzle:a, owner:vv, lastOwner: vv, question:"I have another question", notes:"NOTES", createTime: oldest)
-        def h_h = new Hint(player:pl, puzzle:b, owner:ss, question:"I have another question", notes:"NOTES", createTime: yesterday)
-        def h_i = new Hint(player:pl, puzzle:b, owner:dd, lastOwner: ss, question:"I have another question", notes:"NOTES", createTime: oldest)
-        h_a.save()
-        h_b.save()
-        h_c.save()
-        h_d.save()
-        h_e.save()
-        h_f.save()
-        h_g.save()
-        h_h.save()
-        h_i.save()
-
-        def ts = new Player(name: "Tom", password:"3")
-        ts.save()
     }
     def destroy = {
     }
